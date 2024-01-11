398c048b46526e45ab1a4e784107dcffc3cc93dc
Jack Maclennan <jackwmaclennan@gmail.com>
2024-01-11 18:36:55 +1300

    add Nav.tsx + NavPage.tsx

---
+++ src/components/page/NavPage.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+import Nav from './Nav';
+
+export default function Page({ children } : { children: ReactNode }) {
+  return (
+    <div className="flex flex-col max-h-screen min-h-screen bg-amber-50">
+      <Nav />
+      <main className="flex justify-center w-full flex-grow-0">
+        {children}
+      </main>
+    </div>
+  );
+}
