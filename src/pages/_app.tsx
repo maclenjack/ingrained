392261c0f7cd0f5d91d08e97b4943c87928a9f15
Jack Maclennan <jackwmaclennan@gmail.com>
2024-01-11 17:37:47 +1300

    refactor import of font awesome icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+// import Font Awesome CSS
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
